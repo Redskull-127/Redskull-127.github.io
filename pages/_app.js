357952f7acc72e462890196f692cc09c28d1dcce
Redskull-127 <meerraja17@gmail.com>
2022-05-19 16:49:25 +0530

    Meta Init

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=no"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossOrigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"></link>
     </Head>

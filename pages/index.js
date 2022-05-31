76204c69d63be377160db238a1e68240ca43b019
Redskull-127 <meerraja17@gmail.com>
2022-05-31 23:07:23 +0530

    Advancment to touch devices

--- pages/index.js
+++ pages/index.js
@@ -11,63 +11,56 @@ import "aos/dist/aos.css";
 import React, { useEffect, useState, Component } from "react";
 import { CustomCursor } from "react-svg-cursor";
 
-
-
 export default function Home() {
   const [device, setDevice] = useState(false);
   useEffect(() => {
     Aos.init({ duration: 2000 });
     var userAgent = navigator.userAgent.toLowerCase();
     var Android = userAgent.indexOf("android") > -1;
-    if(Android){
-      setDevice(true)
+    if (Android) {
+      setDevice(true);
     }
   }, []);
-  
 
-  
   return (
     <React.Fragment>
-      <><CustomCursor
-      component={"images/cursor2.png"}
-      isDisabled={device}
-      width={50}
-      height={50}
-      zIndex={420}
-      transform="translate(-32%, -10%) rotateZ(0deg)"
-    /></>
+      <>
+        <CustomCursor
+          component={"images/cursor2.png"}
+          isDisabled={device}
+          width={50}
+          height={50}
+          zIndex={420}
+          transform="translate(-32%, -10%) rotateZ(0deg)" />
+      </>
       <div className={styles.container}>
-        <Head>
-          <title>Meer Tarbani</title>
-          <meta name="description" content="Portfolio Website" />
-          <link rel="icon" href="/images/favicon.ico" />
-        </Head>
-        <main className={styles.main} id="mains" >
-          <Header />
-          <About />
-          <img
-            src="images/emote2.png"
-            loading="lazy"
-            data-aos="fade-down"
-            className={emotes.emote}
-          />
-          <Position />
-          <img
-            src="images/emote3.png"
-            loading="lazy"
-            data-aos="fade-down"
-            className={emotes.emote}
-          />
-          <Projects />
-          <img
-            src="images/emote4.png"
-            loading="lazy"
-            data-aos="fade-down"
-            className={emotes.emote}
-          />
-        </main>
-      </div>
-      <Footer />
+      <Head>
+        <title>Meer Tarbani</title>
+        <meta name="description" content="Portfolio Website" />
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
+      <main className={styles.main} id="mains">
+        <Header />
+        <About />
+        <img
+          src="images/emote2.png"
+          loading="lazy"
+          data-aos="fade-down"
+          className={emotes.emote} />
+        <Position />
+        <img
+          src="images/emote3.png"
+          loading="lazy"
+          data-aos="fade-down"
+          className={emotes.emote} />
+        <Projects />
+        <img
+          src="images/emote4.png"
+          loading="lazy"
+          data-aos="fade-down"
+          className={emotes.emote} />
+      </main>
+    </div><Footer />
     </React.Fragment>
   );
 }

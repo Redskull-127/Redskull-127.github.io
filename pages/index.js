39f2c005ada2b295ac57870490b102006074ba49
Meer Tarbani <76103614+Redskull-127@users.noreply.github.com>
2022-06-08 22:00:39 +0530

    Keywords Added

--- pages/index.js
+++ pages/index.js
@@ -33,39 +33,47 @@ export default function Home() {
           width={50}
           height={50}
           zIndex={420}
-          transform="translate(-32%, -10%) rotateZ(0deg)" />
+          transform="translate(-32%, -10%) rotateZ(0deg)"
+        />
       </>
       <div className={styles.container}>
-      <Head>
-        <title>Meer Tarbani</title>
-        <meta name="description" content="Portfolio Website" />
-        <link rel="icon" href="/images/favicon.ico" />
-      </Head>
-      <main className={styles.main} id="mains">
-        <Header />
-        <About />        
-        <img
-          src="images/emote2.png"
-          loading="lazy"
-          data-aos="fade-down"
-          className={emotes.emote} />
-        <Position />
-        <img
-          src="images/emote3.png"
-          loading="lazy"
-          data-aos="fade-down"
-          className={emotes.emote} />
-        <Projects />
-        <img
-          src="images/emote4.png"
-          loading="lazy"
-          data-aos="fade-down"
-          className={`${styles.emote4}`} />
-        <Quote />
-        <Testing />
-      </main>
-    </div>
-    <Footer />
+        <Head>
+          <title>Meer Tarbani</title>
+          <meta name="description" content="Meer Tarbani Portfolio Website" />
+          <meta
+            name="keywords"
+            content="Meer Tarbani, Meer, Tarbani, Portfolio, Website, meer tarbani, meer tarbani acid, meer tarbani as a fraction, meer tarbani ba, meer tarbani bali, meer tarbani bangla, meer tarbani bangla lyrics, meer tarbani battery, meer tarbani bank, meer tarbani blood pressure, meer tarbani chords, meer tarbani com, meer tarbani code, meer tarbani center, meer tarbani de, meer tarbani delhi, meer tarbani dei, meer tarbani dit, meer tarbani definition, meer tarbani download, meer tarbani disease, meer tarbani english translation, meer tarbani english lyrics, meer tarbani english, meer tarbani english pdf, meer tarbani english subtitles, meer tarbani englisch, meer tarbani example, meer tarbani film, meer tarbani facebook, meer tarbani full movie, meer tarbani fakaza, meer tarbani font, meer tarbani file, meer tarbani for sale,  eer tarbani gif, meer tarbani google translate,  meer tarbani google scholar, meer tarbani google, meer tarbani google drive, meer tarbani games, meer tarbani germany,"
+          />
+          <link rel="icon" href="/images/favicon.ico" />
+        </Head>
+        <main className={styles.main} id="mains">
+          <Header />
+          <About />
+          <img
+            src="images/emote2.png"
+            loading="lazy"
+            data-aos="fade-down"
+            className={emotes.emote}
+          />
+          <Position />
+          <img
+            src="images/emote3.png"
+            loading="lazy"
+            data-aos="fade-down"
+            className={emotes.emote}
+          />
+          <Projects />
+          <img
+            src="images/emote4.png"
+            loading="lazy"
+            data-aos="fade-down"
+            className={`${styles.emote4}`}
+          />
+          <Quote />
+          <Testing />
+        </main>
+      </div>
+      <Footer />
     </React.Fragment>
   );
 }

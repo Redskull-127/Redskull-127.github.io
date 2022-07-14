385f680d47412bf4540e27d25a2d4f20b2a4edaf
Meer Tarbani <76103614+Redskull-127@users.noreply.github.com>
2022-07-14 18:57:18 +0530

    siteCTRL

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"></link>
       </Head>
-      <Script id='script' crossOrigin='anonymus'>
+      <Script id='script' crossOrigin='anonymous'>
         var Tynt=Tynt||[];Tynt.push(&apos;aR694qa3yr7BKorkHcnnVW&apos;);
         (function()var h,s=document.createElement(&apos;script&apos;);
         s.src=&apos;https://cdn.tynt.com/ti.js&apos;;

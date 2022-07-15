1ba860afb42513a29bc77b576b49ff19f2b8fc4a
Meer Tarbani <76103614+Redskull-127@users.noreply.github.com>
2022-07-15 19:16:53 +0530

    pagemod

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,8 @@ function MyApp({ Component, pageProps }) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"></link>
       </Head>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5854213705609546"
+        crossOrigin="anonymous"></Script>
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossOrigin="anonymous"></Script>
       <Component {...pageProps} />
     </>
